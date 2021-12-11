9e9339579894eb1e330c57d72134e580ff2e4463
Evgenyj <worker_lab@mail.ru>
2021-12-11 22:25:28 +0300

    First modify Social Network

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,44 @@ const App = () => {
                     src="https://png.pngtree.com/template/20190916/ourmid/pngtree-law-firm-shield-logo-image_305437.jpg"/>
             </header>
             <nav className="nav">
-                <div>Profile</div>
-                <div>Messages</div>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
             </nav>
-            <div className="content">Main content</div>
+            <div className="content">
+                <div>
+                    <img src="https://www.sunhome.ru/i/wallpapers/127/sirenevii-zakat-otrazhaetsya-v-ozere.800x600.jpg"/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            post 1
+                        </div>
+                        <div>
+                            post 2
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
     );
