0be49219957d9b4c010f84ec19d917cdec8e8bd8
Evgenyj <worker_lab@mail.ru>
2022-01-12 21:58:07 +0300

    Lesson 15 Samuray - final

---
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import s from './Post.module.css';
+
+const Post = () => {
+    return (
+        <div className={s.item}>
+            <img src="https://get.wallhere.com/photo/1680x1050-px-action-adventure-alien-aliens-Avatar-Disney-fantasy-fi-fighting-futuristic-sci-warrior-1635309.jpg"/>
+            post 1
+            <div>
+                <span>like</span>
+            </div>
+
+        </div>
+    )
+}
+
+export default Post
