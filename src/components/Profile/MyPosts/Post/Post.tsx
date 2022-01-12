ae4a78d955806e7bb25ab4ec06afba980b837f23
Evgenyj <worker_lab@mail.ru>
2022-01-12 22:10:51 +0300

    Lesson 16 Samuray - props

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import s from './Post.module.css';
 
-const Post = () => {
+type PropsType = {
+    message: string
+}
+
+const Post = (props: PropsType) => {
     return (
         <div className={s.item}>
             <img src="https://get.wallhere.com/photo/1680x1050-px-action-adventure-alien-aliens-Avatar-Disney-fantasy-fi-fighting-futuristic-sci-warrior-1635309.jpg"/>
-            post 1
+            {props.message}
             <div>
                 <span>like</span>
             </div>
