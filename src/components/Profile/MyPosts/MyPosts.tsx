819c581af253835435b17882c81c96bdfae41513
Evgenyj <worker_lab@mail.ru>
2022-01-12 22:18:01 +0300

    Lesson 16 Samuray - props - final

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -11,8 +11,8 @@ const MyPosts = () => {
                 <button>Add post</button>
             </div>
             <div className={s.posts}>
-                <Post message="Hi, how are you"/>
-                <Post message="It's my first post"/>
+                <Post message="Hi, how are you" likesCount={5}/>
+                <Post message="It's my first post" likesCount={15}/>
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -3,15 +3,17 @@ import s from './Post.module.css';
 
 type PropsType = {
     message: string
+    likesCount: number
 }
 
 const Post = (props: PropsType) => {
     return (
         <div className={s.item}>
-            <img src="https://get.wallhere.com/photo/1680x1050-px-action-adventure-alien-aliens-Avatar-Disney-fantasy-fi-fighting-futuristic-sci-warrior-1635309.jpg"/>
+            <img
+                src="https://get.wallhere.com/photo/1680x1050-px-action-adventure-alien-aliens-Avatar-Disney-fantasy-fi-fighting-futuristic-sci-warrior-1635309.jpg"/>
             {props.message}
             <div>
-                <span>like</span>
+                <span>likes: {props.likesCount}</span>
             </div>
 
         </div>
