c12ecffd9b786e07352bbe80d7736f28c60f9503
Evgenyj <worker_lab@mail.ru>
2022-01-17 22:59:55 +0300

    Lesson 20 Samuray - NavLink - final

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,20 +5,20 @@ import {NavLink} from "react-router-dom";
 const Navbar = () => {
     return (
         <nav className={s.nav}>
-            <div className={s.item}>
-                <NavLink to="/profile">Profile</NavLink>
+            <div>
+                <NavLink to="/profile" className = { navData => navData.isActive ? s.active : s.item }>Profile</NavLink>
             </div>
-            <div className={`${s.item} ${s.active}`}>
-                <NavLink to="/dialogs">Messages</NavLink>
+            <div>
+                <NavLink to="/dialogs" className = { navData => navData.isActive ? s.active : s.item }>Messages</NavLink>
             </div>
-            <div className={s.item}>
-                <NavLink to="/news">News</NavLink>
+            <div>
+                <NavLink to="/news" className = { navData => navData.isActive ? s.active : s.item }>News</NavLink>
             </div>
-            <div className={s.item}>
-                <NavLink to="/music">Music</NavLink>
+            <div>
+                <NavLink to="/music" className = { navData => navData.isActive ? s.active : s.item }>Music</NavLink>
             </div>
-            <div className={s.item}>
-                <NavLink to="/settings">Settings</NavLink>
+            <div>
+                <NavLink to="/settings" className = { navData => navData.isActive ? s.active : s.item }>Settings</NavLink>
             </div>
         </nav>
     )
