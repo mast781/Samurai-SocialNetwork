a568805c6afd22eaff378dd6ac3fccc3bd78c7ec
Evgenyj <worker_lab@mail.ru>
2022-01-19 21:38:21 +0300

    Lesson 21 Samuray - Dialogs - progress

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,29 +4,31 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div className="dialogs">
-            <div className="dialogs-items">
-                <div className="dialog">
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
                     Dimych
                 </div>
-                <div className="dialog">
+                <div className={s.dialog}>
                     Andrey
                 </div>
-                <div className="dialog">
+                <div className={s.dialog}>
                     Sveta
                 </div>
-                <div className="dialog">
+                <div className={s.dialog}>
                     Sasha
                 </div>
-                <div className="dialog">
+                <div className={s.dialog}>
                     Victor
                 </div>
-                <div className="dialog">
+                <div className={s.dialog}>
                     Valera
                 </div>
             </div>
-            <div className="messages">
-                div.message
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your it-kamasutra?</div>
+                <div className={s.message}>Yo</div>
             </div>
         </div>
     )
