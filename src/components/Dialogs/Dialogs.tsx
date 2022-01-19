c3679e919a6fe7cc00f5f71002432d1b24168a5f
Evgenyj <worker_lab@mail.ru>
2022-01-19 21:21:08 +0300

    Lesson 21 Samuray - Dialogs - start

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,8 +4,30 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className="dialogs">
+            <div className="dialogs-items">
+                <div className="dialog">
+                    Dimych
+                </div>
+                <div className="dialog">
+                    Andrey
+                </div>
+                <div className="dialog">
+                    Sveta
+                </div>
+                <div className="dialog">
+                    Sasha
+                </div>
+                <div className="dialog">
+                    Victor
+                </div>
+                <div className="dialog">
+                    Valera
+                </div>
+            </div>
+            <div className="messages">
+                div.message
+            </div>
         </div>
     )
 }
