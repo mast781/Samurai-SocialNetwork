a8d91878175c56ce3210dfc72b7e5202b63d35e3
Evgenyj <worker_lab@mail.ru>
2022-02-13 20:32:30 +0300

    Lesson 23 Samuray - DialogProps - start

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,14 +2,27 @@ import React from "react";
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
+type DialogItemPropsType = {
+    name: string
+    id: string
+}
+
+const DialogItem = (props: DialogItemPropsType) => {
+    return (
+        <div className={s.dialog + ' ' + s.active}>
+            <NavLink to={'/dialogs/'+ props.id}>{props.name}</NavLink>
+        </div>
+    )
+}
 
 const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog + ' ' + s.active}>
+                {/*<div className={s.dialog + ' ' + s.active}>
                     <NavLink to="/dialogs/1">Dimych</NavLink>
-                </div>
+                </div>*/}
+                <DialogItem name='Dimych' id='1'/>
                 <div className={s.dialog}>
                     <NavLink to="/dialogs/2">Andrey</NavLink>
                 </div>
