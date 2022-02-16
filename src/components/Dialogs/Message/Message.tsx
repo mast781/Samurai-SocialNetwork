119d998b403369c26e06a8cabb82979bae8bb373
Evgenyj <worker_lab@mail.ru>
2022-02-16 19:47:35 +0300

    Lesson 26 Samuray - структура проекта - start

---
+++ src/components/Dialogs/Message/Message.tsx
@@ -0,0 +1,12 @@
+import s from "../Dialogs.module.css";
+import React from "react";
+
+type MessagePropsType = {
+    id: number
+    message: string
+}
+export const Message = (props: MessagePropsType) => {
+    return (
+        <div className={s.message}>{props.message}</div>
+    )
+}
